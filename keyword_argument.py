0fa80d05a4b7c0e05e1981d0d50753cbac45de84
Mrinal <waliamrinal@gmail.com>
2018-01-09 00:55:20 +0530

    adding these new python files

--- keyword_argument.py
+++ keyword_argument.py
@@ -1,4 +1,7 @@
 def any_sentence(name='abhi',cast='walia',middle_name='singh'):
-	print(name,middle_name,cast)
-	
+	print(name.title(),middle_name.title(),cast.title())
+
 any_sentence()
+any_sentence(name='mrinal')
+any_sentence(name='anish',cast='aluwalia')
+any_sentence(name='anish',cast='jat')
